e1018a97a8c11526e01fe9bd476c64aab7a4da62
Tomas Novau <tnovau@gmail.com>
2020-04-11 23:30:38 +0200

    Reinjecting an updated manifest

--- project/workbox-config.js
+++ project/workbox-config.js
@@ -1,8 +1,15 @@
 module.exports = {
   "globDirectory": "build/",
   "globPatterns": [
-    "**/*.css"
+    "**/*.css",
+    "index.html",
+    "js/animation.js",
+    "images/home/*.jpg",
+    "images/icon/*.svg"
   ],
   "swDest": "build/sw.js",
-  "swSrc": "src/sw.js"
+  "swSrc": "src/sw.js",
+  "globIgnores": [
+    "../workbox-config.js"
+  ]
 };
