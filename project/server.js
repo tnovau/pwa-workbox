470e5d555c0e2ae2abd12f965881ae4ac4b87166
Tomas Novau <tnovau@gmail.com>
2020-04-11 19:02:15 +0200

    Basic service worker

--- project/server.js
+++ project/server.js
@@ -19,7 +19,7 @@ const app = express();
 // This serves static files from the specified directory
 app.use(express.static(__dirname + '/build'));
 
-const server = app.listen(8081, () => {
+const server = app.listen(3000, () => {
 
   const host = server.address().address;
   const port = server.address().port;
